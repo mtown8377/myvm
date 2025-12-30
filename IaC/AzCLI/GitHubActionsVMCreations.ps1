a7db80640d8a54643e756ef4769b1b83998b7a8f
mtown8377 <keith-johnston@me.com>
2025-12-30 10:34:18 -0500

    5th commit

--- IaC/AzCLI/GitHubActionsVMCreations.ps1
+++ IaC/AzCLI/GitHubActionsVMCreations.ps1
@@ -71,16 +71,6 @@ Write-Output "Done"
 Write-Output ""
 #endregion
 
-#region Create Resource Group
-# This creates the resource group used to house the VM
-Write-Output "Creating resource group $resourceGroupName in region $resourceGroupNameRegion..."
-az group create `
-    --name $resourceGroupName `
-    --location $resourceGroupNameRegion
-    Write-Output "Done creating resource group"
-    Write-Output ""
- #endregion
-
 #region Create VM
 # Create a VM in the resource group
 Write-Output "Creating VM..."
